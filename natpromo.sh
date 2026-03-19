b0bf10ecd3533518bd9b656318eb8c482a4202f1
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2026-03-19 14:02:35 -0500

    Update natpromo.sh

--- natpromo.sh
+++ natpromo.sh
@@ -9,8 +9,14 @@ cp nat-router.sh /root/nat-router.sh || {
   echo "Copy failed"
   exit 1
 }
+echo "Choose your local network and public Internet Interfaces"
+echo
+ip -4 addr | awk '/^[0-9]+:/ {iface=$2} /inet / {print iface, $2}'
+read -p "Enter local network interface name(Not the IP or number after /):" LAN_IF
+read -p "Enter public Internet network interface name:" WAN_IF  
+echo "LAN_IF=$LAN_IF\n" > /etc/nat-router.conf
+echo "WAN_IF=$WAN_IF\n" >> /etc/nat-router.conf
 
-nano /root/nat-router.sh
 chmod +x /root/nat-router.sh
 
 cp nat-router.service /etc/systemd/system/nat-router.service
@@ -18,3 +24,8 @@ cp nat-router.service /etc/systemd/system/nat-router.service
 systemctl daemon-reload
 systemctl enable nat-router.service
 systemctl start nat-router.service
+if [ "$?" -eq 0 ]; then
+echo "Router was successfully installed. Set the computers Default gateway to the ip address of this computer."
+exit 0
+fi
+echo "Failed see the lines above"
