68b6281f196ee6fbec3f43fb6552bd7cd2c5081e
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2026-03-19 14:10:35 -0500

    Add checks for LAN_IF and WAN_IF variables
    Updated NAT router script to check for missing interface variables and adjusted iptables rules.

--- nat-router.sh
+++ nat-router.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Simple NAT router for Lubuntu
 source /etc/nat-router.conf
+if [ -z "$LAN_IF"]; then
+echo "LAN_IF is missing"
+exit 1
+fi
+if [ -z "$WAN_IF"]; then
+echo "wAN_IF is missing"
+exit 1
+fi
 echo "[+] Enabling IP forwarding"
 sysctl -w net.ipv4.ip_forward=1
-
 echo "[+] Flushing old iptables rules"
 iptables -F
 iptables -t nat -F
@@ -15,7 +22,7 @@ iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 
 echo "[+] Allow forwarding from LAN to WAN"
-iptables -A FORWARD -i "$LAN_IF" -o "$WAN_IF" -s "$LAN_NET" -j ACCEPT
+iptables -A FORWARD -i "$LAN_IF" -o "$WAN_IF" -j ACCEPT
 
 echo "[+] Allow established/related connections back in"
 iptables -A FORWARD -i "$WAN_IF" -o "$LAN_IF" \
